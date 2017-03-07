d83c44eca63d129efda95f49eca4aaf934849cd0
Lee Ngo <lee.ngo@gmail.com>
2017-03-07 11:40:02 -0800

    Deleting custom.js files

--- custom.js
+++ custom.js
@@ -1,11 +1,1 @@
-function goldOrPotion() {
-    var response = document.getElementById("response").value;
-
-    if (response === 'TAKE GOLD') {
-      document.getElementById("gold").classList.toggle('hidden');
-      document.getElementById("intro").classList.toggle('hidden');
-    } else if (response == 'DRINK POTION') {
-      document.getElementById("potion").classList.toggle('hidden');
-      document.getElementById("intro").classList.add('hidden').toggle('hidden');
-    } else { document.getElementById("error").innerHTML = "Sorry, I do not understand." }
-}
+// Your JavaScript code goes here! Remember to Inspect Element and check the Console whenever possible.
